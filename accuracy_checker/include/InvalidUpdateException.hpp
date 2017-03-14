89ed891ad83d8366e6dc573dfc3034d7e1ce085e
Luca Morreale <luca.morreale93@gmail.com>
2017-03-14 14:37:18 +0100

    Created runtime error

---
+++ accuracy_checker/include/InvalidUpdateException.hpp
@@ -0,0 +1,21 @@
+
+#ifndef MESH_ACCURACY_INVALID_UPDATE_EXCEPTION_H
+#define MESH_ACCURACY_INVALID_UPDATE_EXCEPTION_H
+
+#include <stdexcept>
+
+namespace meshac {
+
+    class InvalidUpdateException : public std::runtime_error {
+    public:
+        InvalidUpdateException(std::string msg) : std::runtime_error(msg)
+        { /*    */ }
+
+        InvalidUpdateException() : std::runtime_error("Invalid Updated performed.")
+        { /*    */ }
+
+    };
+
+} // namespace meshac
+
+#endif // MESH_ACCURACY_INVALID_UPDATE_EXCEPTION_H

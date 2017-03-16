b9d31668ae48fdbe201fd21c17375cd8badb5355
Luca Morreale <luca.morreale93@gmail.com>
2017-03-16 12:50:21 +0100

    Created abstract class to estimate matrix variance of a single 3D point

---
+++ accuracy_checker/include/Point3DVarianceEstimator.hpp
@@ -0,0 +1,43 @@
+
+#ifndef MESH_ACCURACY_POINT_3D_VARIANCE_ESTIMATOR_H
+#define MESH_ACCURACY_POINT_3D_VARIANCE_ESTIMATOR_H
+
+#include <glm/gtc/epsilon.hpp>
+
+#include <alias_definition.hpp>
+#include <AccuracyModel.hpp>
+#include <meshac_type_definition.hpp>
+
+
+namespace meshac {
+
+    class Point3DVarianceEstimator {
+    public:
+        Point3DVarianceEstimator(AccuracyModelPtr accuracyModel, GLMListVec3 points);
+        ~Point3DVarianceEstimator();
+
+        virtual EigMatrix computeVariaceForPoint(GLMVec3 point);
+
+        void setAccuracyModel(AccuracyModelPtr accuracyModel);
+        AccuracyModelPtr getAccuracyModel();
+
+        GLMListVec3 get3DPoints();
+        void set3DPoints(GLMListVec3 points);
+        void append3DPoints(GLMListVec3 points);
+        void append3DPoints(GLMVec3 points);
+
+    protected:
+        virtual EigMatrix selectVarianceMatrix(EigMatrixList &mat) = 0;
+
+    private:
+        AccuracyModelPtr accuracyModel;
+        GLMListVec3 points;
+
+        const GLMVec3 EPSILON = GLMVec3(SENSIBILITY);
+
+    };
+    
+} // namespace meshac
+
+
+#endif // MESH_ACCURACY_POINT_3D_VARIANCE_ESTIMATOR_H
---
+++ accuracy_checker/src/Point3DVarianceEstimator.cpp
@@ -0,0 +1,64 @@
+
+#include <Point3DVarianceEstimator.hpp>
+
+namespace meshac {
+
+    Point3DVarianceEstimator::Point3DVarianceEstimator(AccuracyModelPtr accuracyModel, GLMListVec3 points)
+    {
+        this->accuracyModel = accuracyModel;
+        this->points = points;
+    }
+
+    Point3DVarianceEstimator::~Point3DVarianceEstimator()
+    {
+        delete accuracyModel;
+        points.clear();
+    }
+
+    EigMatrix Point3DVarianceEstimator::computeVariaceForPoint(GLMVec3 point)
+    {
+        GLMListVec3 points = this->get3DPoints();
+
+        EigMatrixList variances;
+        
+        for (int i = 0; i<points.size(); i++) {
+            if (glm::epsilonEqual(points[i], point, EPSILON)[0]) {
+                variances = this->getAccuracyModel()->getAccuracyForPoint(i);
+            }
+        }
+
+        return selectVarianceMatrix(variances);
+    }
+
+    void Point3DVarianceEstimator::setAccuracyModel(AccuracyModelPtr accuracyModel)
+    {
+        this->accuracyModel = accuracyModel;
+    }
+
+    AccuracyModelPtr Point3DVarianceEstimator::getAccuracyModel()
+    {
+        return this->accuracyModel;
+    }
+
+    GLMListVec3 Point3DVarianceEstimator::get3DPoints()
+    {
+        return this->points;
+    }
+    
+    void Point3DVarianceEstimator::set3DPoints(GLMListVec3 points)
+    {
+        this->points = points;
+    }
+
+    void Point3DVarianceEstimator::append3DPoints(GLMListVec3 newPoints)
+    {
+        this->points.insert(this->points.end(), newPoints.begin(), newPoints.end());
+    }
+
+    void Point3DVarianceEstimator::append3DPoints(GLMVec3 newPoint)
+    {
+        this->points.push_back(newPoint);
+    }
+
+
+} // namespace meshac

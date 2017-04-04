7074afc2997057f7af0a8d13bdddd09ead785da4
Luca Morreale <luca.morreale93@gmail.com>
2017-04-04 13:04:13 +0200

    Added aliases

--- accuracy_checker/include/meshac/alias_definition.hpp
+++ accuracy_checker/include/meshac/alias_definition.hpp
@@ -21,6 +21,7 @@ typedef std::vector<IntList> IntArrayList;
 typedef std::vector<std::string> StringList;
 
 /* Shortcuts for OpenCV types */
+typedef cv::Vec2f CVVector2;
 typedef std::vector<cv::Vec2f> CVListVec2;
 typedef cv::Mat CVMat;
 
@@ -34,24 +35,27 @@ typedef std::vector<GLMVec3> GLMListVec3;
 typedef std::vector<std::map<int, glm::vec2>> ListMappingGLMVec2;
 
 /* Shortcuts for Eigen types */
-typedef Eigen::VectorXf EigVector;
-typedef Eigen::Vector3f EigVector3;
-typedef Eigen::Vector4f EigVector4;
-typedef Eigen::MatrixXf EigMatrix;
-typedef Eigen::Matrix3f EigMatrix3;
-typedef Eigen::Matrix4f EigMatrix4;
-typedef Eigen::Matrix<float, 3, 4> EigCameraMatrix;
-typedef Eigen::Matrix<float, 3, 2> EigJacobianMatrix;
+typedef Eigen::VectorXd EigVector;
+typedef Eigen::Vector2d EigVector2;
+typedef Eigen::Vector3d EigVector3;
+typedef Eigen::Vector4d EigVector4;
+typedef Eigen::MatrixXd EigMatrix;
+typedef Eigen::Matrix3d EigMatrix3;
+typedef Eigen::Matrix4d EigMatrix4;
+typedef Eigen::Matrix<double, 3, 4> EigCameraMatrix;
+typedef Eigen::Matrix<double, 3, 2> EigJacobianMatrix;
 
+typedef std::vector<EigVector3> EigVector3List;
+typedef std::vector<EigVector> EigVectorList;
 typedef std::vector<EigMatrix> EigMatrixList;
 
-inline EigMatrix EigIdentity(int n) { return Eigen::MatrixXf::Identity(n, n); };
+inline EigMatrix EigIdentity(int n) { return Eigen::MatrixXd::Identity(n, n); };
 
-inline EigMatrix EigZeros(int n) { return Eigen::MatrixXf::Zero(n, n); };
-inline EigMatrix EigZeros(int rows, int cols) { return Eigen::MatrixXf::Zero(rows, cols); };
+inline EigMatrix EigZeros(int n) { return Eigen::MatrixXd::Zero(n, n); };
+inline EigMatrix EigZeros(int rows, int cols) { return Eigen::MatrixXd::Zero(rows, cols); };
 
-inline EigMatrix EigOnes(int n) { return Eigen::MatrixXf::Ones(n, n); };
-inline EigMatrix EigOnes(int rows, int cols) { return Eigen::MatrixXf::Ones(rows, cols); };
+inline EigMatrix EigOnes(int n) { return Eigen::MatrixXd::Ones(n, n); };
+inline EigMatrix EigOnes(int rows, int cols) { return Eigen::MatrixXd::Ones(rows, cols); };
 
 
 /* Shortcut for manifoldReconstructor types */

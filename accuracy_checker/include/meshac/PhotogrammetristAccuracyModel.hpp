5194cbba55848e620289517e21bfc832120743eb
Luca Morreale <luca.morreale93@gmail.com>
2017-04-12 13:40:56 +0200

    Updated header

--- accuracy_checker/include/meshac/PhotogrammetristAccuracyModel.hpp
+++ accuracy_checker/include/meshac/PhotogrammetristAccuracyModel.hpp
@@ -1,7 +1,7 @@
 #ifndef MESH_ACCURACY_PHOTOGRAMMETRIST_ACCURACY_MODEL_H
 #define MESH_ACCURACY_PHOTOGRAMMETRIST_ACCURACY_MODEL_H
 
-#include <manifoldReconstructor/SfMData.h>
+#include <realtimeMR/SfMData.h>
 
 #include <meshac/AccuracyModel.hpp>
 #include <meshac/alias_definition.hpp>

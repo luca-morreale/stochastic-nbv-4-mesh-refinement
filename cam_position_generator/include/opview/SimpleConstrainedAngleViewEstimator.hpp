c059d740cc58483f18bc8d872a9802a49935e903
Luca Morreale <luca.morreale93@gmail.com>
2017-04-23 21:12:54 +0200

    Added generation of constraint to the model

--- cam_position_generator/include/opview/SimpleConstrainedAngleViewEstimator.hpp
+++ cam_position_generator/include/opview/SimpleConstrainedAngleViewEstimator.hpp
@@ -13,16 +13,21 @@
 
 namespace opview {
 
+    #define BD_RAPPORT 0.7
+
     class SimpleConstrainedAngleViewEstimator : public OptimalViewEstimator {
     public:
-        SimpleConstrainedAngleViewEstimator(Delaunay3 &dt);
+        SimpleConstrainedAngleViewEstimator(Delaunay3 &dt, GLMVec3List &cams);
         ~SimpleConstrainedAngleViewEstimator();
 
     protected:
         virtual GLMVec3 determingBestPositionForFace(Face face);
 
         virtual void buildModel(IloModel &model, IloEnv env, Face &face);
-        virtual IloExpr createProblem(PointD3 &centroid, CGALVec3 &targetDirection, IloEnv env);
+        virtual void addGoal(IloModel &model, PointD3 &barycenter, CGALVec3 &normVector, IloEnv env);
+        virtual IloExpr createGoalExpression(PointD3 &centroid, CGALVec3 &targetDirection, IloEnv env);
+        virtual void addConstraints(IloModel &model, PointD3 &barycenter);
+        virtual IloRangeList createConstraints(PointD3 &centroid);
         virtual GLMVec3 extractSolutions(IloCplex &cplex);
 
 
@@ -31,6 +36,7 @@ namespace opview {
 
     private:
         IloNumVarList goalPoint;
+        GLMVec3List cams;
         
     };   
 }   // namespace opview
--- cam_position_generator/src/SimpleConstrainedAngleViewEstimator.cpp
+++ cam_position_generator/src/SimpleConstrainedAngleViewEstimator.cpp
@@ -3,89 +3,16 @@
 
 namespace opview {
 
-    SimpleConstrainedAngleViewEstimator::SimpleConstrainedAngleViewEstimator(Delaunay3 &dt) : OptimalViewEstimator(dt)
+    SimpleConstrainedAngleViewEstimator::SimpleConstrainedAngleViewEstimator(Delaunay3 &dt, GLMVec3List &cams) : OptimalViewEstimator(dt)
     {
-        //optimizationEnv = new IloEnvironment();
-        //cplex = new IloCplex(optimizationEnv);
+        this->cams = cams;
     }
 
     SimpleConstrainedAngleViewEstimator::~SimpleConstrainedAngleViewEstimator()
     {
-        //optimizationEnv->end();
-
-        //delete cplex;
-        //delete optimizationEnv;
+        this->cams.clear();
     }
-    /*
-    GLMVec3 SimpleConstrainedAngleViewEstimator::determingBestPositionForFace(Face face)
-    {
-        IloEnv optimizationEnv;
-        IloCplex cplex;
-        IloModel model(optimizationEnv);
-        IloNumVar x1(optimizationEnv, -500.0, 500.0, ILOFLOAT);     // lowerbound 0, upperbound 40 and type continuous
-
-
-
-
-
-        model.add(IloMaximize(optimizationEnv, logVonMises(0, 40)));      // added goal to minimize
-        //model.add(-x1 + x2 + x3 <= 20);     // create constraint
-
-        
-        //IloCplex cplex(model);
-        cplex.extract(model);
-        cplex.solve();
-
-        //IloNum val1 = cplex.getValue(x1);
-
-        //IloNum objval = cplex.getObjValue();
-
-        //cplex.clearModel();
-
-        optimizationEnv.end();
-    }
-
-    IloExpr logBessel0(IloExpr x)   // log of I0(x) as approximately x − 1/2 log(2 *pi * x)     https://math.stackexchange.com/questions/376758/exponential-approximation-of-the-modified-bessel-function-of-first-kind-equatio
-    {
-        IloExpr logArg = 2 * M_PI * x;
-        return x - 0.5 * IloLog(logArg);
-    }
-
-    IloExpr logVonMises(IloNumVar alpha, IloNumVar kappa)   // alpha should be for sure an expression, kappa can even be a number!
-    {
-        return alpha * kappa - log(2 * M_PI) - logBessel0(kappa);
-    }
-
-    IloExpr logVonMises(EigVector n, EigVector v, EigVector goalDirection)
-    {
-        IloNumVar x1(optimizationEnv, -500.0, 500.0, ILOFLOAT);     // lowerbound 0, upperbound 40 and type continuous
-        IloNumVar x1(optimizationEnv, -500.0, 500.0, ILOFLOAT);     // lowerbound 0, upperbound 40 and type continuous
-        IloNumVar x1(optimizationEnv, -500.0, 500.0, ILOFLOAT);     // lowerbound 0, upperbound 40 and type continuous
-    }
-
-
-    IloExpr logVonMises(EigVector n, EigVector v, EigVector goalDirection)
-    {
-        double dotProduct = n.dot(v);
-        double normProduct = n.norm() * v.norm();
-        double alpha = dotProduct / normProduct;
-
-        dotProduct = n.dot(goalDirection);
-        normProduct = n.norm() * goalDirection.norm();
-        double k = dotProduct / normProduct;
-
-        return logVonMises(alpha, k);
-    }
-
-    IloExpr logVonMises(EigVector n, EigVector v, double k)
-    {
-        auto dotProduct = n.dot(v);
-        auto normProduct = n.norm() * v.norm();
-        double alpha = dotProduct / normProduct;
-
-        return logVonMises(alpha, k);
-    }
-    */
+    
 
     GLMVec3 SimpleConstrainedAngleViewEstimator::determingBestPositionForFace(Face face)
     {
@@ -108,12 +35,17 @@ namespace opview {
         CGALVec3 normVector = this->normalVectorToFace(face);
         PointD3 barycenter = this->barycenterVectorToFace(face.face);
 
-        IloExpr goalExp = createProblem(barycenter, normVector, env);
-        model.add(IloMaximize(env, goalExp));      // added goal to minimize
+        this->addConstraints(model, barycenter);
+        this->addGoal(model, barycenter, normVector, env);
     }
 
+    void SimpleConstrainedAngleViewEstimator::addGoal(IloModel &model, PointD3 &barycenter, CGALVec3 &normVector, IloEnv env)
+    {
+        IloExpr goalExp = createGoalExpression(barycenter, normVector, env);
+        model.add(IloMaximize(env, goalExp));
+    }
 
-    IloExpr SimpleConstrainedAngleViewEstimator::createProblem(PointD3 &centroid, CGALVec3 &targetDirection, IloEnv env)
+    IloExpr SimpleConstrainedAngleViewEstimator::createGoalExpression(PointD3 &centroid, CGALVec3 &targetDirection, IloEnv env)
     {
         IloNumVarList point = convertVector(3, env);
         this->setGoalPoint(point);
@@ -123,6 +55,25 @@ namespace opview {
 
         return numerator / denominator;
     }
+
+    void SimpleConstrainedAngleViewEstimator::addConstraints(IloModel &model, PointD3 &barycenter)
+    {
+        IloRangeList constraintList = this->createConstraints(barycenter);
+        for (IloRange constraint : constraintList) {
+            model.add(constraint);
+        }
+    }
+
+    IloRangeList SimpleConstrainedAngleViewEstimator::createConstraints(PointD3 &centroid)
+    {
+        IloRangeList constraintList;
+        for (int i = 0; i < cams.size(); i++) {
+            IloExpr pointDistance = sqrtDistance(goalPoint, cams[i]);
+            constraintList.push_back(pointDistance >= BD_RAPPORT * BD_RAPPORT);
+        }
+        return constraintList;
+    }
+
 
     GLMVec3 SimpleConstrainedAngleViewEstimator::extractSolutions(IloCplex &cplex)
     {

152556af150e28e0d3cef4bfce6307ed8ab012c9
Luca Morreale <luca.morreale93@gmail.com>
2017-04-30 22:21:13 +0200

    Updated class extending base classes

--- cam_position_generator/include/opview/SimpleConstrainedAngleViewEstimator.hpp
+++ cam_position_generator/include/opview/SimpleConstrainedAngleViewEstimator.hpp
@@ -8,39 +8,18 @@
 
 
 #include <opview/alias_definition.h>
-#include <opview/OptimalViewEstimator.hpp>
+#include <opview/DistanceCameraViewEstimator.hpp>
 #include <opview/CplexVectorOperations.hpp>
 
 namespace opview {
 
-    #define BD_RAPPORT 0.7
-
-    class SimpleConstrainedAngleViewEstimator : public OptimalViewEstimator {
+    class SimpleConstrainedAngleViewEstimator : public DistanceCameraViewEstimator {
     public:
         SimpleConstrainedAngleViewEstimator(GLMVec3List &cams);
         ~SimpleConstrainedAngleViewEstimator();
 
     protected:
-        virtual GLMVec3 determingBestPositionForFace(Face &face);
-
-        virtual void buildModel(IloModel model, Face &face);
-        virtual void setUpGoalVariables(IloEnv env);
-        virtual void addGoal(IloModel model, CGALVec3 &normVector, CGALVec3 &centroid);
         virtual IloExpr createGoalExpression(CGALVec3 &targetDirection, CGALVec3 &centroid, IloEnv env);
-        virtual void addConstraints(IloModel model, CGALVec3 &barycenter);
-        virtual IloConstraintList createConstraints(CGALVec3 &centroid, IloEnv env);
-        virtual GLMVec3 extractSolutions(IloCplex &cplex);
-
-
-        IloNumVarList getGoalPoint();
-        void setGoalPoint(IloNumVarList &point);
-
-    private:
-        IloNumVarList goalPoint;
-        CGALVec3List cams;
-
-        IloExpr middleToCenterDistanceExpr(CGALVec3 &cam, CGALVec3 &center, IloEnv env);
-        IloConstraint createBDConstraint(CGALVec3 &cam, CGALVec3 &center, IloEnv env);
         
     };
 
--- cam_position_generator/src/SimpleConstrainedAngleViewEstimator.cpp
+++ cam_position_generator/src/SimpleConstrainedAngleViewEstimator.cpp
@@ -1,76 +1,17 @@
-
 #include <opview/SimpleConstrainedAngleViewEstimator.hpp>
 
 namespace opview {
 
-    SimpleConstrainedAngleViewEstimator::SimpleConstrainedAngleViewEstimator(GLMVec3List &cams) : OptimalViewEstimator()
-    {
-        this->cams = convertListToCGALVecList(cams);
-        
-    }
+    SimpleConstrainedAngleViewEstimator::SimpleConstrainedAngleViewEstimator(GLMVec3List &cams) : DistanceCameraViewEstimator(cams)
+    { /*    */ }
 
     SimpleConstrainedAngleViewEstimator::~SimpleConstrainedAngleViewEstimator()
-    {
-        this->cams.clear();
-    }
-
-    GLMVec3 SimpleConstrainedAngleViewEstimator::determingBestPositionForFace(Face &face)
-    {
-        IloEnv optimizationEnv;
-        IloModel model(optimizationEnv);
-
-        std::cout << "starting building model\n";
-        this->buildModel(model, face);
-        std::cout << "model built\n";
-        IloCplex cplex(optimizationEnv);
-        
-        std::cout << "created solver\n";
-        cplex.setParam(IloCplex::Threads, 4);
-        cplex.setOut(optimizationEnv.getNullStream());
-
-        try {
-            cplex.extract(model);
-            std::cout << "added model\n";
-            cplex.solve();
-        }catch (IloException& e) {
-            std::cout << e << std::endl;
-            return GLMVec3();
-        }
-
-        std::cout << "solution " << cplex.getStatus() << std::endl;
-
-        GLMVec3 solution = this->extractSolutions(cplex);
-
-        optimizationEnv.end();
-        return solution;
-    }
-
-    void SimpleConstrainedAngleViewEstimator::buildModel(IloModel model, Face &face)
-    {
-        CGALVec3 normVector = this->normalVectorToFace(face);
-        PointD3 barycenter = this->barycenterVectorToFace(face.face);
-        CGALVec3 centroid = convertPoinToCGALVec(barycenter);
-
-        this->setUpGoalVariables(model.getEnv());
-
-        this->addConstraints(model, centroid);
-        this->addGoal(model, normVector, centroid);
-    }
-
-    void SimpleConstrainedAngleViewEstimator::setUpGoalVariables(IloEnv env)
-    {
-        IloNumVarList point = convertVector(3, env);
-        this->setGoalPoint(point);
-    }
-
-    void SimpleConstrainedAngleViewEstimator::addGoal(IloModel model, CGALVec3 &normVector,CGALVec3 &centroid)
-    {
-        IloExpr goalExp = createGoalExpression(normVector, centroid, model.getEnv());
-        model.add(IloMaximize(model.getEnv(), goalExp));
-    }
+    { /*    */ }
 
     IloExpr SimpleConstrainedAngleViewEstimator::createGoalExpression(CGALVec3 &targetDirection, CGALVec3 &centroid, IloEnv env)
     {
+        IloNumVarList goalPoint = this->getGoalPoint();
+        
         IloNumExprArray vectorPointCenter = differenceExpr(goalPoint, centroid, env);
         IloExpr numerator = dotExpr(targetDirection, vectorPointCenter, env);
         IloExpr denominator = sqrtNormExpr(vectorPointCenter, env);
@@ -79,68 +20,5 @@ namespace opview {
         //return IloSquare(numerator);
         return numerator;
     }
-
-    void SimpleConstrainedAngleViewEstimator::addConstraints(IloModel model, CGALVec3 &barycenter)
-    {
-        IloConstraintList constraintList = this->createConstraints(barycenter, model.getEnv());
-        for (IloConstraint constraint : constraintList) {
-            model.add(constraint);
-        }
-    }
-
-    IloConstraintList SimpleConstrainedAngleViewEstimator::createConstraints(CGALVec3 &centroid, IloEnv env)
-    {
-        IloConstraintList constraintList;
-
-        for (int i = 0; i < cams.size(); i++) {
-            constraintList.push_back(createBDConstraint(cams[i], centroid, env));
-        }
-        // constraint that limit the point in space, otherwise goes to the infinite
-        //constraintList.push_back(pyramidDistanceExpr(goalPoint, centroid, env) <= 100.0);
-        constraintList.push_back(manhattanDistanceExpr(goalPoint, centroid, env) <= 100.0);
-        
-
-        return constraintList;
-    }
-
-    IloConstraint SimpleConstrainedAngleViewEstimator::createBDConstraint(CGALVec3 &cam, CGALVec3 &center, IloEnv env)
-    {
-        //IloExpr distanceToCam = pyramidDistanceExpr(goalPoint, cam, env);
-        IloExpr distanceToCam = manhattanDistanceExpr(goalPoint, cam, env);
-        IloExpr rightSide = BD_RAPPORT * BD_RAPPORT * middleToCenterDistanceExpr(cam, center, env);
-
-        return distanceToCam >= rightSide;
-    }
-
-    IloExpr SimpleConstrainedAngleViewEstimator::middleToCenterDistanceExpr(CGALVec3 &cam, CGALVec3 &center, IloEnv env)
-    {
-        IloExprList mp = middlePoint(goalPoint, cam, env);
-        //return pyramidDistanceExpr(mp, center, env);
-        return manhattanDistanceExpr(mp, center, env);
-    }
-
-
-    GLMVec3 SimpleConstrainedAngleViewEstimator::extractSolutions(IloCplex &cplex)
-    {
-        std::cout << "trying to extract the solution\n";
-        GLMVec3 solution;
-        for (int i = 0; i < this->goalPoint.size(); i++) {
-            std::cout << "value " << cplex.getValue(this->goalPoint[i]) << std::endl;
-            solution[i] = cplex.getValue(this->goalPoint[i]);
-        }
-        std::cout << "extracted solution\n";
-        return solution;
-    }
-
-    IloNumVarList SimpleConstrainedAngleViewEstimator::getGoalPoint()
-    {
-        return this->goalPoint;
-    }
-
-    void SimpleConstrainedAngleViewEstimator::setGoalPoint(IloNumVarList &point)
-    {
-        this->goalPoint = point;
-    }
-
 
 } // namespace opview

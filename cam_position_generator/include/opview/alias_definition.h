a79e4eb1db6fec2ee39bb4cd9964bcbe65f9c4aa
Luca Morreale <luca.morreale93@gmail.com>
2017-05-22 12:08:58 +0200

    Removed some unused aliases

--- cam_position_generator/include/opview/alias_definition.h
+++ cam_position_generator/include/opview/alias_definition.h
@@ -23,26 +23,8 @@ namespace opview {
     typedef std::vector<GLMVec3> GLMVec3List;
 
     /* Shortcuts for Eigen types */
-    typedef Eigen::VectorXd EigVector;
-    typedef Eigen::Vector2d EigVector2;
-    typedef Eigen::Vector3d EigVector3;
-    typedef Eigen::Vector4d EigVector4;
-    typedef Eigen::MatrixXd EigMatrix;
-    typedef Eigen::Matrix3d EigMatrix3;
-    typedef Eigen::Matrix4d EigMatrix4;
-    typedef Eigen::Matrix<double, 3, 4> EigCameraMatrix;
+    typedef Eigen::Matrix<float, 5, 1> EigVector5;
 
-    typedef std::vector<EigVector3> EigVector3List;
-    typedef std::vector<EigVector> EigVectorList;
-    typedef std::vector<EigMatrix> EigMatrixList;
-
-    inline EigMatrix EigIdentity(int n) { return Eigen::MatrixXd::Identity(n, n); };
-
-    inline EigMatrix EigZeros(int n) { return Eigen::MatrixXd::Zero(n, n); };
-    inline EigMatrix EigZeros(int rows, int cols) { return Eigen::MatrixXd::Zero(rows, cols); };
-
-    inline EigMatrix EigOnes(int n) { return Eigen::MatrixXd::Ones(n, n); };
-    inline EigMatrix EigOnes(int rows, int cols) { return Eigen::MatrixXd::Ones(rows, cols); };
 
     /* Shortcuts for CGAL types */
     typedef std::vector<PointD3> PointD3List;

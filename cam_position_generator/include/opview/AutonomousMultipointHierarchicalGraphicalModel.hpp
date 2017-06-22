664ff21ae14f197c2b57b336bff516b47129f937
Luca Morreale <luca.morreale93@gmail.com>
2017-06-22 12:32:41 +0200

    Fixed autonomouse multipoint GM

--- cam_position_generator/include/opview/AutonomousMultipointHierarchicalGraphicalModel.hpp
+++ cam_position_generator/include/opview/AutonomousMultipointHierarchicalGraphicalModel.hpp
@@ -13,26 +13,13 @@ namespace opview {
                                                         size_t maxPoints, long double maxUncertainty, double goalAngle=55, double dispersion=5);
         ~AutonomousMultipointHierarchicalGraphicalModel();
 
-        virtual void updateMeshInfo(int pointIndex, GLMVec3 point, GLMVec3 normal, double accuracy) override;
-        virtual void updateMeshInfo(int pointIndex, GLMVec3 normal, double accuracy) override;
-        virtual void updateMeshInfo(int pointIndex, double accuracy) override;
-        virtual void addPoint(GLMVec3 point, GLMVec3 normal, double uncertainty) override;
-
         virtual void estimateBestCameraPosition();
 
     protected:
-        virtual int checkWorstPoint();
-        virtual void setWorstPoint();
-
         // bring up overloaded function from parent
         using MultipointHierarchicalGraphicalModel::estimateBestCameraPosition;
-        
-        virtual void updateWorstReference(int pointIndex, double accuracy);
 
     private:
-        long double worstUncertainty;
-        int worstPointIndex;
-
         typedef MultipointHierarchicalGraphicalModel super;
 
     };

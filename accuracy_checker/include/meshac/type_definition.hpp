b1914f22ddf71aab381d716e93ddf75bfadb809b
Luca Morreale <luca.morreale93@gmail.com>
2017-06-27 15:10:45 +0200

    Accuracy: change custom type of face of mesh

--- accuracy_checker/include/meshac/type_definition.hpp
+++ accuracy_checker/include/meshac/type_definition.hpp
@@ -1,17 +1,31 @@
 #ifndef MESH_ACCURACY_TYPE_DEFINITION_H
 #define MESH_ACCURACY_TYPE_DEFINITION_H
 
+#include <vector>
 
 namespace meshac {
 
     const float SENSIBILITY = 0.001f;
 
-    struct Face {
-        FaceVertices face;
-        PointD3 oppositeVertex;
-    };
+    typedef std::array<size_t, 3> SizeT3Array;
 
-    typedef std::vector<Face> FaceList;
+    typedef struct FaceIndex {
+        SizeT3Array vs;
+
+        FaceIndex() { /*    */ }
+        FaceIndex(size_t x, size_t y, size_t z)
+        {
+            vs[0] = x; vs[1] = y; vs[2] = z;
+        }
+
+        void set(int pos, size_t index)
+        {
+            vs[pos] = index;
+        }
+
+    } FaceIndex;
+
+    typedef std::vector<FaceIndex> FaceIndexList;
     
 } // namesace meshac
 

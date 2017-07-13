2b9a5d13807ac33222fb304133ea19a7ae1b4211
Luca Morreale <luca.morreale93@gmail.com>
2017-07-13 11:20:47 +0200

    Optimal Cam: reduce particle for resampling

--- cam_position_generator/test/src/main_mcmc.cpp
+++ cam_position_generator/test/src/main_mcmc.cpp
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
     glm::vec3 normal(-0.14341, 0.238821, -0.960416);
     // // centroid, normal
 
-    opview::MCConfiguration mcConfig(10, 1000000, 100); // size_t resamplingNum, size_t particles, size_t particlesUniform
+    opview::MCConfiguration mcConfig(10, 100000, 100); // size_t resamplingNum, size_t particles, size_t particlesUniform
     // maybe is better if less point in uniform? and then increase in the case of mc?
     
     opview::MCMCCamGenerator model(camConfig, meshFile, cams, mcConfig);

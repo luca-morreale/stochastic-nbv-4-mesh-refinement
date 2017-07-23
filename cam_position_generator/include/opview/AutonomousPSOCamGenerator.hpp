cb423469e2e498a93e71b73189da56abb59f6a77
Luca Morreale <luca.morreale93@gmail.com>
2017-07-23 11:55:18 +0200

    Optimal Cam: create autonomous pso cam generator

---
+++ cam_position_generator/include/opview/AutonomousPSOCamGenerator.hpp
@@ -0,0 +1,67 @@
+#ifndef CAM_POSITION_GENERATOR_AUTONOMOUS_PSO_CAM_GENERATOR_H
+#define CAM_POSITION_GENERATOR_AUTONOMOUS_PSO_CAM_GENERATOR_H
+
+#include <opview/type_definition.h>
+#include <opview/alias_definition.h>
+#include <opview/PSOCamGenerator.hpp>
+#include <opview/utilities.hpp>
+
+namespace opview {
+
+    class AutonomousPSOCamGenerator : public PSOCamGenerator {
+    public:
+        AutonomousPSOCamGenerator(CameraGeneralConfiguration &camConfig, MeshConfiguration &meshConfig, 
+                                            PSOConfiguration &psoConfig, size_t maxPoints, long double maxUncertainty, 
+                                            double goalAngle=55, double dispersion=8);
+
+        ~AutonomousPSOCamGenerator();
+
+        virtual void updateMeshInfo(int pointIndex, GLMVec3 point, GLMVec3 normal, double accuracy);
+        virtual void updateMeshInfo(int pointIndex, GLMVec3 normal, double accuracy);
+        virtual void updateMeshInfo(int pointIndex, double accuracy);
+        virtual void addPoint(GLMVec3 point, GLMVec3 normal, double uncertainty);
+
+        virtual void estimateBestCameraPosition();
+        
+    protected:
+        virtual GLMVec3List getPoints();
+        virtual GLMVec3List getNormals();
+        virtual DoubleList getUncertainties();
+
+        virtual void setupWorstPoints();
+        virtual void updateWorstPoints(int index, long double uncertainty);
+        virtual void retainWorst();
+        virtual DoubleIntList getWorstPointsList();
+
+        virtual double estimateForWorstPointSeen(EigVector5 &pose, BoostObjFunction function);
+        virtual double computeWeightForPoint(int pointIndex);
+
+        virtual LabelType logVonMisesWrapper(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normal);
+        virtual LabelType visibilityDistribution(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normalVector);
+        virtual LabelType imageProjectionDistribution(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normalVector);
+
+        using MCMCCamGenerator::estimateBestCameraPosition;
+
+    private:
+        GLMVec3List points;
+        GLMVec3List normals;
+        DoubleList uncertainty;
+        DoubleIntList worstPointsList;
+        long double SUM_UNCERTAINTY;
+
+        size_t maxPoints;
+        long double maxUncertainty;
+
+        void precomputeSumUncertainty();
+
+        LabelType parentCatllToLogVonMises(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normal);
+        LabelType parentCallToVisibilityEstimation(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normalVector);
+        LabelType parentCallToPlaneDistribution(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normalVector);
+
+        typedef MCMCCamGenerator super;
+
+    };
+
+} // namespace opview
+
+#endif // CAM_POSITION_GENERATOR_AUTONOMOUS_PSO_CAM_GENERATOR_H
---
+++ cam_position_generator/src/AutonomousPSOCamGenerator.cpp
@@ -0,0 +1,168 @@
+#include <opview/AutonomousPSOCamGenerator.hpp>
+
+
+namespace opview {
+
+    AutonomousPSOCamGenerator::AutonomousPSOCamGenerator(CameraGeneralConfiguration &camConfig, MeshConfiguration &meshConfig, 
+                                                            PSOConfiguration &psoConfig, size_t maxPoints, long double maxUncertainty, double goalAngle, double dispersion)
+                                                            : PSOCamGenerator(camConfig, meshConfig.filename, meshConfig.cams, psoConfig, goalAngle, dispersion)
+    {
+        this->points = meshConfig.points;
+        this->normals = meshConfig.normals;
+        this->uncertainty = meshConfig.uncertainty;
+        this->maxPoints = maxPoints;
+        this->maxUncertainty = maxUncertainty;
+
+        precomputeSumUncertainty();
+        setupWorstPoints();
+
+        this->getLogger()->resetFile("auto_pso.json");
+    }
+    
+    AutonomousPSOCamGenerator::~AutonomousPSOCamGenerator()
+    { /*    */ }
+
+    void AutonomousPSOCamGenerator::precomputeSumUncertainty()
+    {
+        SUM_UNCERTAINTY = 0.0;
+        for (int p = 0; p < uncertainty.size(); p++) {
+            SUM_UNCERTAINTY += uncertainty[p];
+        }
+    }
+
+    double AutonomousPSOCamGenerator::computeWeightForPoint(int pointIndex)
+    {
+        return (double)uncertainty[pointIndex] / (double)SUM_UNCERTAINTY;
+    }
+
+    void AutonomousPSOCamGenerator::estimateBestCameraPosition()
+    {
+        size_t worstPointIndex = worstPointsList[0].first;
+        GLMVec3 worstCentroid = this->points[worstPointIndex];
+        GLMVec3 normal = this->normals[worstPointIndex];
+        this->estimateBestCameraPosition(worstCentroid, normal);
+    }
+
+    LabelType AutonomousPSOCamGenerator::logVonMisesWrapper(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normal)
+    {
+        return estimateForWorstPointSeen(pose, boost::bind(&AutonomousPSOCamGenerator::parentCatllToLogVonMises, this, _1, _2, _3));
+    }
+
+    LabelType AutonomousPSOCamGenerator::visibilityDistribution(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normalVector)
+    {
+        return estimateForWorstPointSeen(pose, boost::bind(&AutonomousPSOCamGenerator::parentCallToVisibilityEstimation, this, _1, _2, _3));
+    }
+
+    LabelType AutonomousPSOCamGenerator::imageProjectionDistribution(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normalVector)
+    {
+        return estimateForWorstPointSeen(pose, boost::bind(&AutonomousPSOCamGenerator::parentCallToPlaneDistribution, this, _1, _2, _3));
+    }
+
+    double AutonomousPSOCamGenerator::estimateForWorstPointSeen(EigVector5 &pose, BoostObjFunction function)
+    {
+        LabelType val = 0.0;
+        #pragma omp parallel for
+        for (int i = 0; i < worstPointsList.size(); i++) {
+            DoubleIntPair el = worstPointsList[i];
+            double normalizedWeight = computeWeightForPoint(el.second);     // more the uncertainty is high more important it is seen
+            LabelType local_val = function(pose, points[el.second], normals[el.second]);
+            
+            #pragma omp critical
+            val += local_val * normalizedWeight;
+        }
+        
+        return val;
+    }
+
+    LabelType AutonomousPSOCamGenerator::parentCatllToLogVonMises(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normal)
+    {
+        return super::logVonMisesWrapper(pose, centroid, normal);
+    }
+
+    LabelType AutonomousPSOCamGenerator::parentCallToVisibilityEstimation(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normalVector)
+    {
+        return super::visibilityDistribution(pose, centroid, normalVector);
+    }
+
+    LabelType AutonomousPSOCamGenerator::parentCallToPlaneDistribution(EigVector5 &pose, GLMVec3 &centroid, GLMVec3 &normalVector)
+    {
+        return super::imageProjectionDistribution(pose, centroid, normalVector);
+    }
+
+    void AutonomousPSOCamGenerator::updateMeshInfo(int pointIndex, GLMVec3 point, GLMVec3 normal, double uncertainty)
+    {
+        this->points[pointIndex] = point;
+        updateMeshInfo(pointIndex, normal, uncertainty);
+        updateWorstPoints(pointIndex, uncertainty);
+    }
+
+    void AutonomousPSOCamGenerator::updateMeshInfo(int pointIndex, GLMVec3 normal, double uncertainty)
+    {
+        this->normals[pointIndex] = normal;
+        updateMeshInfo(pointIndex, uncertainty);
+        updateWorstPoints(pointIndex, uncertainty);
+    }
+
+    void AutonomousPSOCamGenerator::updateMeshInfo(int pointIndex, double uncertainty)
+    {
+        SUM_UNCERTAINTY += uncertainty - this->uncertainty[pointIndex];
+        this->uncertainty[pointIndex] = uncertainty;
+        updateWorstPoints(pointIndex, uncertainty);
+    }
+
+    void AutonomousPSOCamGenerator::addPoint(GLMVec3 point, GLMVec3 normal, double uncertainty)
+    {
+        this->points.push_back(point);
+        this->normals.push_back(normal);
+        this->uncertainty.push_back(uncertainty);
+        SUM_UNCERTAINTY += uncertainty;
+        updateWorstPoints(this->points.size()-1, uncertainty);
+    }
+
+    void AutonomousPSOCamGenerator::setupWorstPoints()
+    {
+        worstPointsList.clear();
+        #pragma omp parallel for        // not much to gain using parallelism
+        for (int p = 0; p < points.size(); p++) {
+            if (uncertainty[p] > this->maxUncertainty) {
+                #pragma omp critical
+                worstPointsList.push_back(std::make_pair(uncertainty[p], p));
+            }
+        }
+        retainWorst();
+    }
+
+    void AutonomousPSOCamGenerator::updateWorstPoints(int index, long double uncertainty)
+    {
+        worstPointsList.push_back(std::make_pair(uncertainty, index));
+        retainWorst();
+    }
+
+    void AutonomousPSOCamGenerator::retainWorst()
+    {
+        std::sort(worstPointsList.rbegin(), worstPointsList.rend());
+        int eraseFrom = std::min(this->maxPoints, worstPointsList.size());
+        worstPointsList.erase(worstPointsList.begin() + eraseFrom, worstPointsList.end());
+    }
+
+    DoubleIntList AutonomousPSOCamGenerator::getWorstPointsList()
+    {
+        return worstPointsList;
+    }  
+
+    GLMVec3List AutonomousPSOCamGenerator::getPoints()
+    {
+        return points;
+    }
+
+    GLMVec3List AutonomousPSOCamGenerator::getNormals()
+    {
+        return normals;
+    }
+    
+    DoubleList AutonomousPSOCamGenerator::getUncertainties()
+    {
+        return uncertainty;
+    }
+
+} // namespace opview

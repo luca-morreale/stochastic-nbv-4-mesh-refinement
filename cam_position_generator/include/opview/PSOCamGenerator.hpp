c0795e5742afb6fd2b86eda669157f3bed9f3c75
Luca Morreale <luca.morreale93@gmail.com>
2017-07-28 16:28:25 +0200

    Optimal Cam: change guard of pso class and add virtual specifier

--- cam_position_generator/include/opview/PSOCamGenerator.hpp
+++ cam_position_generator/include/opview/PSOCamGenerator.hpp
@@ -1,5 +1,5 @@
-#ifndef CAM_POSITION_GENERATOR_PSO_SAMPLE_GENERATOR_H
-#define CAM_POSITION_GENERATOR_PSO_SAMPLE_GENERATOR_H
+#ifndef CAM_POSITION_GENERATOR_PSO_CAM_GENERATOR_H
+#define CAM_POSITION_GENERATOR_PSO_CAM_GENERATOR_H
 
 #include <gsl/gsl_sf_gamma.h>
 #include <gsl/gsl_rng.h>
@@ -14,9 +14,6 @@
 
 namespace opview {
 
-    #define COORDINATE_SIZE 3
-    #define ORIENTATION_SIZE 5
-
     class PSOCamGenerator : public MCMCCamGenerator {
     public:
         PSOCamGenerator(CameraGeneralConfiguration &camConfig, std::string &meshFile, GLMVec3List &cams, 
@@ -28,27 +25,29 @@ namespace opview {
 
     protected:
 
-        void convertSamplesToParticles(OrderedPose &currentOptima);
-        void updateParticles(GLMVec3 &centroid, GLMVec3 &normVector);
-        void updateVelocityParticle(int j);
-        void updatePositionParticle(int j);
-        void evaluateSwarm(GLMVec3 &centroid, GLMVec3 &normVector);
-        void updateSwarmValues(DoubleList &values);
+        virtual void convertSamplesToParticles(OrderedPose &currentOptima);
+        virtual void updateParticles(GLMVec3 &centroid, GLMVec3 &normVector);
+        virtual void updateVelocityParticle(int p);
+        virtual void updatePositionParticle(int p);
+        virtual void evaluateSwarm(GLMVec3 &centroid, GLMVec3 &normVector);
+        virtual void updateSwarmValues(DoubleList &values);
 
-        void fixSpaceVelocity(int p);
-        void fixSpacePosition(int p);
+        virtual void fixSpaceVelocity(int p);
+        virtual void fixSpacePosition(int p);
 
-        double uniform();
+        virtual double uniform();
+        virtual EigVector5 randVector();
+
+        ParticleList particles;
+        EigVector5 inertiaWeight;
+        EigVector5 c1;
+        EigVector5 c2;
         
     private:
         const gsl_rng *randGen;
-        ParticleList particles;
-
+        
         GLMVec3 spaceLowerBounds;
         GLMVec3 spaceUpperBounds;
-
-        EigVector5 omega;
-        EigVector5 phiP;
 
         int bestParticleIndex;
 

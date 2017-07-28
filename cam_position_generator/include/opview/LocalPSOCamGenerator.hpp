ba7c5784283767b5dad494ae0f6cc7745c5279bc
Luca Morreale <luca.morreale93@gmail.com>
2017-07-28 16:31:01 +0200

    Optimal Cam: create pso version using local best

---
+++ cam_position_generator/include/opview/LocalPSOCamGenerator.hpp
@@ -0,0 +1,30 @@
+#ifndef CAM_POSITION_GENERATOR_LOCAL_PSO_CAM_GENERATOR_H
+#define CAM_POSITION_GENERATOR_LOCAL_PSO_CAM_GENERATOR_H
+
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_linalg.h>
+
+#include <opview/PSOCamGenerator.hpp>
+
+namespace opview {
+
+    class LocalPSOCamGenerator : public PSOCamGenerator {
+    public:
+        LocalPSOCamGenerator(CameraGeneralConfiguration &camConfig, std::string &meshFile, GLMVec3List &cams, 
+                                            PSOConfiguration &psoConfig, double goalAngle=45, double dispersion=8);
+
+        ~LocalPSOCamGenerator();
+
+    protected:
+
+        virtual void updateVelocityParticle(int p) override;
+    };
+
+    typedef LocalPSOCamGenerator* LocalPSOCamGeneratorPtr;
+
+} // namespace opview
+
+#endif // CAM_POSITION_GENERATOR_LOCAL_PSO_CAM_GENERATOR_H
---
+++ cam_position_generator/src/LocalPSOCamGenerator.cpp
@@ -0,0 +1,35 @@
+#include <opview/LocalPSOCamGenerator.hpp>
+
+namespace opview {
+
+
+    LocalPSOCamGenerator::LocalPSOCamGenerator(CameraGeneralConfiguration &camConfig, std::string &meshFile, GLMVec3List &cams, 
+                                                    PSOConfiguration &psoConfig, double goalAngle, double dispersion)
+                                                    : PSOCamGenerator(camConfig, meshFile, cams, psoConfig, goalAngle, dispersion)
+    { /*    */ }
+
+    LocalPSOCamGenerator::~LocalPSOCamGenerator()
+    { /*    */ }
+
+    void LocalPSOCamGenerator::updateVelocityParticle(int p)
+    {
+        EigVector5 randBestLeft = this->randVector();
+        EigVector5 randBestRight = this->randVector();
+        EigVector5 randPrevious = this->randVector();
+
+        int left = (p - 1 < 0) ? particles.size() - 1 : p - 1;
+        int right = (p + 1 >= particles.size()) ? 0 : p + 1;
+
+        EigVector5 diffPreviousBest = particles[p]->bestPosition - particles[p]->position;
+        EigVector5 diffLeft = particles[left]->position - particles[p]->position;
+        EigVector5 diffRight = particles[right]->position - particles[p]->position;
+
+        particles[p]->velocity = inertiaWeight.cwiseProduct(particles[p]->velocity);
+        particles[p]->velocity += randPrevious.cwiseProduct(c1.cwiseProduct(diffPreviousBest));
+        particles[p]->velocity += randBestLeft.cwiseProduct(c2.cwiseProduct(diffLeft));
+        particles[p]->velocity += randBestRight.cwiseProduct(c2.cwiseProduct(diffRight));
+
+        this->fixSpaceVelocity(p);
+    }
+
+} // namespace opview

59e80e9b7f04eb1d38fa91e932992e8858039af5
Luca Morreale <luca.morreale93@gmail.com>
2017-08-05 18:44:29 +0200

    Accuracy: create file containing camera utilities functions

---
+++ accuracy_checker/include/meshac/camera_utilities.hpp
@@ -0,0 +1,35 @@
+#ifndef MESH_ACCURACY_CAMERA_UTILITIES_H
+#define MESH_ACCURACY_CAMERA_UTILITIES_H
+
+#include <CGAL/exceptions.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/epsilon.hpp>
+#include <glm/gtx/norm.hpp>
+
+#include <meshac/alias_definition.hpp>
+#include <meshac/UnprojectablePointThroughCamException.hpp>
+
+namespace meshac {
+
+    GLMVec2 projectThrough(GLMVec3 &meshPoint, CameraType &P, TreePtr tree);
+    
+    bool isMeaningfulPose(GLMVec3 &meshPoint, CameraType &P, TreePtr tree);
+    bool isOppositeView(GLMVec3 &meshPoint, CameraType &P);
+    bool isIntersecting(GLMVec3 &meshPoint, CameraType &P, TreePtr tree);
+    bool isMathemathicalError(Segment_intersection &intersection, Point &point);
+    bool isPointInsideImage(GLMVec2 &point2D, CameraType &P);
+
+    GLMVec2 getProjectedPoint(GLMVec3 &meshPoint, CameraMatrix &P);
+    int getImageWidth(CameraType &P);
+    int getImageHeight(CameraType &P);
+    GLMVec3 getCameraCenter(CameraType &P);
+    CameraMatrix getExtrinsicMatrix(CameraType &P);
+    RotationMatrix getRotationMatrix(CameraType &P);
+    CameraMatrix getCameraMatrix(CameraType &P);
+
+    extern const GLMVec4 zdir;
+
+}
+
+#endif // MESH_ACCURACY_CAMERA_UTILITIES_H
---
+++ accuracy_checker/src/camera_utilities.cpp
@@ -0,0 +1,112 @@
+#include <meshac/camera_utilities.hpp>
+
+namespace meshac {
+
+    const GLMVec4 zdir = GLMVec4(0.0f, 0.0f, 1.0f, 0.0f);
+
+    GLMVec2 projectThrough(GLMVec3 &meshPoint, CameraType &P, TreePtr tree)
+    {
+        GLMVec2 point = getProjectedPoint(meshPoint, P.cameraMatrix);
+        
+        if (!isPointInsideImage(point, P)) {  // fast rejection, fast to compute
+            throw UnprojectablePointThroughCamException();
+        }
+        if (!isMeaningfulPose(meshPoint, P, tree)) {
+            throw UnprojectablePointThroughCamException();
+        }
+        return point;
+    }
+
+    bool isMeaningfulPose(GLMVec3 &meshPoint, CameraType &P, TreePtr tree)
+    {
+        return !isIntersecting(meshPoint, P, tree) && !isOppositeView(meshPoint, P);
+    }
+
+    bool isOppositeView(GLMVec3 &meshPoint, CameraType &P)
+    {
+        CameraMatrix E = getExtrinsicMatrix(P);
+
+        GLMVec4 p = E * GLMVec4(meshPoint, 1.0f);
+        return glm::dot(glm::normalize(p), zdir) < 0.0f;    // if > 0.0 than it sees the object.
+    }
+
+    bool isIntersecting(GLMVec3 &meshPoint, CameraType &P, TreePtr tree)
+    {
+        GLMVec3 pose = getCameraCenter(P);
+        Point cam(pose[0], pose[1], pose[2]);
+        Point point(meshPoint[0], meshPoint[1], meshPoint[2]);
+        
+        try {
+            Segment segment_query(cam, point);
+            Segment_intersection intersection;
+            
+            #pragma omp critical
+            intersection = tree->any_intersection(segment_query);  // gives the first intersected primitives, so probably the farer one
+            
+            if (intersection) {
+                return !isMathemathicalError(intersection, point);
+            } else {
+                return false;
+            }
+        } catch (const CGAL::Assertion_exception &ex) {
+            return true;
+        }
+    }
+
+    bool isMathemathicalError(Segment_intersection &intersection, Point &point)
+    {
+        const Point* intersectedPoint = boost::get<Point>(&(intersection->first));
+        if(intersectedPoint) {
+            return CGAL::squared_distance(*intersectedPoint, point) < 0.0001f;
+        }
+        return false;
+    }
+
+    bool isPointInsideImage(GLMVec2 &point2D, CameraType &P)
+    {
+        return point2D.x < (float)getImageWidth(P) && point2D.x > 0.0f && point2D.y < (float)getImageHeight(P) && point2D.y > 0.0f;
+    }
+
+    GLMVec2 getProjectedPoint(GLMVec3 &meshPoint, CameraMatrix &P)
+    {
+        GLMVec4 point3D = GLMVec4(meshPoint, 1.0f);
+        P = glm::transpose(P);
+        GLMVec4 point2D = P * point3D;
+
+        point2D = point2D / point2D.z;
+
+        return GLMVec2(point2D.x, point2D.y);
+    }
+
+    int getImageWidth(CameraType &P)
+    {
+        return P.imageWidth;
+    }
+
+    int getImageHeight(CameraType &P)
+    {
+        return P.imageHeight;
+    }
+
+    GLMVec3 getCameraCenter(CameraType &P)
+    {
+        return P.center;
+    }
+
+    CameraMatrix getExtrinsicMatrix(CameraType &P)
+    {
+        CameraMatrix cam = CameraMatrix(getRotationMatrix(P));
+        return glm::translate(cam, getCameraCenter(P));
+    }
+
+    RotationMatrix getRotationMatrix(CameraType &P)
+    {
+        return P.rotation;
+    }
+
+    CameraMatrix getCameraMatrix(CameraType &P)
+    {
+        return P.cameraMatrix;
+    }
+
+}

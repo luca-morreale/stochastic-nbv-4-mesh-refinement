cd8094be0722953b2c2710d66ffb4cf728b4344e
Luca Morreale <luca.morreale93@gmail.com>
2017-08-11 23:45:27 +0200

    Eval: add convetion from pose to position with look at in pipeline evaluation

--- evaluation_cam/main/pipeline.cpp
+++ evaluation_cam/main/pipeline.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 #include <utilities.hpp>
 #include <aliases.h>
@@ -10,6 +11,8 @@
 
 void log(std::string msg);
 std::string convert(std::string &poseFile);
+std::string transformToLookat(std::string &fixedPosefile);
+std::vector<std::string> transform(std::vector<std::string> &blocks);
 
 int main(int argc, char **argv) {
 
@@ -31,11 +34,14 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    fixedPosefile = transformToLookat(fixedPosefile);
+
     std::string convertedPosesFile = "output/output_transform.txt";
     std::string closestPosesFile = "output/output_closest.txt";
     std::string outputEvaluation = "output/output_evaluation.txt";
+    std::string command;
 
-    std::string command = "./transform b " + fixedPosefile + " " + convertedPosesFile;
+    command = "./transform b " + fixedPosefile + " " + convertedPosesFile;
     system(command.c_str());
 
     command = "./closest output_mapping.txt " + convertedPosesFile + " " + closestPosesFile;
@@ -60,6 +66,52 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+std::string transformToLookat(std::string &fixedPosefile)
+{
+    std::ifstream cin(fixedPosefile);
+    std::stringstream stream;
+
+    while(!cin.eof()) {
+        std::string line;
+        cin >> line;
+        if (line.size() == 0) continue;
+
+        std::vector<std::string> blocks;
+        boost::split(blocks, line, boost::is_any_of("_"));
+
+        blocks = transform(blocks);
+        for (int i = 0; i < blocks.size(); i++) {
+            stream << blocks[i] << " ";
+        }
+    }
+
+    cin.close();
+
+    std::string newFile = "converted_poses.txt";
+    std::ofstream cout(newFile);
+    cout << stream.str();
+    cout.close();
+
+    return newFile;
+}
+
+std::vector<std::string> transform(std::vector<std::string> &blocks)
+{
+    std::vector<std::string> list(blocks.begin(), blocks.end());
+    float roll = 0;
+    float pitch = strtod(blocks[3].c_str(), NULL);
+    float yaw = strtod(blocks[4].c_str(), NULL);
+    glm::mat3 rot = cameval::rotationMatrix(roll, pitch, yaw);
+
+    glm::vec3 zdir(0.0f, 0.0f, 5.0f);
+    zdir = rot * zdir;
+
+    list[3] = std::to_string(zdir[0]);
+    list[4] = std::to_string(zdir[1]);
+    list.push_back(std::to_string(zdir[2]));
+    return list;
+}
+
 std::string convert(std::string &poseFile)
 {
     std::string newFile = "converted_poses.txt";
@@ -67,9 +119,10 @@ std::string convert(std::string &poseFile)
     std::ifstream cin(poseFile);
 
     while(!cin.eof()) {
-        std::string x, y, z, lx, ly,lz;
-        cin >> x >> y >> z >> lx >> ly >> lz;
-        cout << x << "_" << y << "_" << z << "_" << lx << "_" << ly << "_" << lz << std::endl;
+        std::string x, y, z, ly,lz;
+        cin >> x >> y >> z >> ly >> lz;
+        if (x.size() == 0 || y.size() == 0 || z.size() == 0 || ly.size() == 0 || lz.size() == 0) continue;
+        cout << x << "_" << y << "_" << z << "_" << ly << "_" << lz << std::endl;
     }
 
     cin.close();

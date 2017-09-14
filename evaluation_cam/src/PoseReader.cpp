295236cfefc665e1bd79ebf39cd1314d20741904
Luca Morreale <luca.morreale93@gmail.com>
2017-09-14 17:00:38 +0200

    Evaluation: fix yaw, pitch and roll generation in PoseReader

--- evaluation_cam/src/PoseReader.cpp
+++ evaluation_cam/src/PoseReader.cpp
@@ -49,15 +49,19 @@ namespace cameval {
         while(!cin.eof()) {
             GLMVec3 position, lookAt;
             cin >> position.x >> position.y >> position.z >> lookAt.x >> lookAt.y >> lookAt.z;
+            if (position.x == 0.0 && position.y == 0.0 && position.z == 0.0)    // posa vuota
+                continue;
 
             ProjectionMatrix view = glm::lookAt(position, lookAt, GLMVec3(0, -1, 0));
             RotationMatrix rot(view);
+            rot = glm::transpose(rot);
             GLMVec3 angle = computeAngles(rot);
 
             this->positions.push_back(position);
             this->rotations.push_back(rot);
             this->angles.push_back(angle);
         }
+
 
         cin.close();
     }
@@ -66,9 +70,9 @@ namespace cameval {
     {
         GLMVec3 angle;
         // NOTE remember to invert rows and cols
-        angle.x = std::atan2(rot[0][1], rot[0][0]);    // roll
-        angle.y = std::atan2(-rot[0][2], std::sqrt(std::pow(rot[1][2],2) + std::pow(rot[2][2], 2)));   // pitch
-        angle.z = std::atan2(rot[1][2], rot[2][2]);    // yaw
+        angle.x = std::atan2(rot[1][0], rot[0][0]);    // roll
+        angle.y = std::atan2(-rot[2][0], std::sqrt(std::pow(rot[2][1],2) + std::pow(rot[2][2], 2)));   // pitch
+        angle.z = std::atan2(rot[2][1], rot[2][2]);    // yaw
 
         if (!this->radians) {
             angle.x = deg(angle.x);

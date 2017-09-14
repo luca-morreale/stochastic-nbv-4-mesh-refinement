6bdbd8f5dfd3ec739a2a4a13b96a9cf0a5514524
Luca Morreale <luca.morreale93@gmail.com>
2017-09-14 17:08:21 +0200

    Evaluation: create python script to visualize coverage

---
+++ evaluation_cam/src/plotter.py
@@ -0,0 +1,24 @@
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import savefig
+
+
+from os import listdir
+from os.path import isfile, join
+
+onlyfiles = [f for f in listdir(".") if isfile(f)]
+onlyfiles = [f for f in onlyfiles if f.find("_coverage.txt") != -1]
+
+for file in onlyfiles:
+
+    with open(file) as stream:
+        content = stream.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    content = [float(x.strip().split(" ")[1]) for x in content] 
+
+    # create the general figure     
+    plt.plot(range(1,11), content)
+    plt.ylabel("% Coverage")
+    plt.xlabel("Distance Threshold")
+    savefig(file + ".pdf")
+    plt.clf()

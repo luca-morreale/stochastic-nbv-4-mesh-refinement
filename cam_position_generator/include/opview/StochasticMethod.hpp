d59d0bac03e21a1c56f3e7e8f61a490786650403
Luca Morreale <luca.morreale93@gmail.com>
2017-09-16 16:08:49 +0200

    Optimal cam: create abstract class for stochastic optimization method
    Create basic abstract class for stochastic optmizatoin method using the formulation to optimize the enegy function

---
+++ cam_position_generator/include/opview/StochasticMethod.hpp
@@ -0,0 +1,85 @@
+#ifndef CAM_POSITION_GENERATOR_STOCHASTIC_METHOD_H
+#define CAM_POSITION_GENERATOR_STOCHASTIC_METHOD_H
+
+#include <opview/alias_definition.h>
+#include <opview/ExhaustiveReportWriter.hpp>
+#include <opview/Formulation.hpp>
+#include <opview/GaussianSampleGenerator.hpp>
+#include <opview/orientation_utilities.hpp>
+#include <opview/type_definition.h>
+#include <opview/utilities.hpp>
+
+namespace opview {
+
+    #define orientationCycles(delta) for(int ptc = 0; ptc < 360; ptc+=delta) for(int yaw = 0; yaw < 360; yaw+=delta)
+
+    
+    class StochasticMethod {
+    public:
+        StochasticMethod(CameraGeneralConfiguration &camConfig, std::string &meshFile, GLMVec3List &cams, 
+                                    StochasticConfiguration &stoConfig, double goalAngle=45, double dispersion=5);
+
+        ~StochasticMethod();
+
+        virtual DoubleList estimateBestCameraPosition(GLMVec3 &centroid, GLMVec3 &normVector) = 0;
+
+        CameraGeneralConfiguration getCamConfig();
+        GLMVec3List getCams();
+        StochasticConfiguration getStochasticConfig();
+        VonMisesConfiguration getVonMisesConfig();
+        ExhaustiveReportWriterPtr getLogger();
+        void setLogger(ExhaustiveReportWriterPtr log);
+        GLMVec3 lowerBounds();
+        GLMVec3 upperBounds();
+        size_t getUniformParticles();
+        size_t getResamplingParticles();
+        size_t getResamplingSteps();
+
+        
+    protected:
+
+        virtual void createTree();
+        TreePtr getTree();
+        FormulationPtr getFormulation();
+
+        virtual OrderedPose uniformSamplingStep(GLMVec3 &centroid, GLMVec3 &normVector, int round);
+        virtual EigVector5List uniformPointsGetter();
+
+        virtual OrderedPose computeEnergyForPoses(EigVector5List &orientedPoints, GLMVec3 &centroid, GLMVec3 &normVector);
+        virtual OrderedPose extractBestResults(OrderedPose &poses, int round);
+        OrderedPose orderPoses(EigVector5List &orientedPoints, DoubleList &values);
+        EigVector5List insertOrientation(GLMVec3List &points);
+        
+
+        void setOffspring(double offspring);
+        double getOffspring();
+
+        float offsetX();
+        float offsetY();
+        float offsetZ();
+        
+        
+    private:
+        CameraGeneralConfiguration camConfig;
+        GLMVec3List cams;
+        std::string meshFile;
+        StochasticConfiguration stoConfig;
+        TreePtr tree;
+        VonMisesConfiguration vonMisesConfig;
+
+        FormulationPtr formulation;
+        ExhaustiveReportWriterPtr log;
+
+        double OFFSPRING;
+
+        
+        OrderedPose convertAngles(OrderedPose poses);
+        
+        Polyhedron extractPolyhedron();
+        TriangleList getTriangleList(Polyhedron &poly);
+        
+    };
+
+} // namespace opview
+
+#endif // CAM_POSITION_GENERATOR_STOCHASTIC_METHOD_H
---
+++ cam_position_generator/src/StochasticMethod.cpp
@@ -0,0 +1,237 @@
+#include <opview/StochasticMethod.hpp>
+
+
+namespace opview {
+    
+    StochasticMethod::StochasticMethod(CameraGeneralConfiguration &camConfig, std::string &meshFile, GLMVec3List &cams, 
+                                            StochasticConfiguration &stoConfig, double goalAngle, double dispersion)
+    {
+        this->stoConfig = stoConfig;
+        this->camConfig = camConfig;
+        this->meshFile = meshFile;
+        this->cams = cams;
+        this->vonMisesConfig = {deg2rad(goalAngle), dispersion};
+        this->log = new ExhaustiveReportWriter("stochastic.json");
+
+        createTree();
+        formulation = new Formulation(vonMisesConfig, camConfig, tree, cams);
+    }
+
+    StochasticMethod::~StochasticMethod()
+    {
+        delete log;
+        delete formulation;
+        delete tree;
+    }
+
+
+
+    OrderedPose StochasticMethod::uniformSamplingStep(GLMVec3 &centroid, GLMVec3 &normVector, int round)
+    {
+        EigVector5List orientedPoints = uniformPointsGetter();
+        OrderedPose poses = computeEnergyForPoses(orientedPoints, centroid, normVector);
+        return this->extractBestResults(poses, round);
+    }
+    
+    EigVector5List StochasticMethod::uniformPointsGetter()
+    {  
+        GLMVec3List points = GaussianSampleGenerator::extractUniformSamples(
+                    {std::make_pair(stoConfig.bounds.lower.x, stoConfig.bounds.upper.x), 
+                        std::make_pair(stoConfig.bounds.lower.y, stoConfig.bounds.upper.y), 
+                        std::make_pair(stoConfig.bounds.lower.z, stoConfig.bounds.upper.z)}, 
+                        stoConfig.particles.uniform);
+
+        return insertOrientation(points);
+    }
+
+    EigVector5List StochasticMethod::insertOrientation(GLMVec3List &points)
+    {
+        EigVector5List orientedPoints;
+
+        #pragma omp parallel for collapse(3)
+        for (int p = 0; p < points.size(); p++) {
+            orientationCycles(stoConfig.particles.deltaDegree) {
+                EigVector5 pose;
+                pose << points[p].x, points[p].y, points[p].z, deg2rad((float)ptc), deg2rad((float)yaw);
+                #pragma omp critical
+                orientedPoints.push_back(pose);
+            }
+        }
+        return orientedPoints;
+    }
+
+    OrderedPose StochasticMethod::computeEnergyForPoses(EigVector5List &orientedPoints, GLMVec3 &centroid, GLMVec3 &normVector)
+    {
+        DoubleList values(orientedPoints.size());
+
+        #pragma omp parallel for
+        for (int i = 0; i < orientedPoints.size(); i++) {
+            values[i] = formulation->computeEnergy(orientedPoints[i], centroid, normVector);
+        }
+
+        return orderPoses(orientedPoints, values);
+    }
+
+    OrderedPose StochasticMethod::orderPoses(EigVector5List &orientedPoints, DoubleList &values)
+    {
+        OrderedPose poses;
+
+        #pragma omp parallel for
+        for (int p = 0; p < orientedPoints.size(); p++) {
+            ValuePose el = ValuePose(values[p], orientedPoints[p]);
+            #pragma omp critical
+            poses.push(el);
+        }
+        return poses;
+    }
+
+    OrderedPose StochasticMethod::extractBestResults(OrderedPose &poses, int round)
+    {
+        OrderedPose optima = copy(poses, (size_t)(OFFSPRING*stoConfig.particles.num));
+        
+        OrderedPose convertedAnglesOptima = convertAngles(optima);
+
+        this->log->append(convertAngles(optima), round);
+
+        return optima;
+    }
+
+    OrderedPose StochasticMethod::convertAngles(OrderedPose poses)
+    {
+        OrderedPose optima;
+        int c = 0;
+        while(!poses.empty() && c < (size_t)(OFFSPRING* getUniformParticles())) {
+            auto tmp = poses.top();
+            tmp.second[3] = rad2deg(tmp.second[3]);
+            tmp.second[4] = rad2deg(tmp.second[4]);
+            optima.push(tmp);
+            poses.pop();
+            c++;
+        }
+        return optima;
+    }
+
+    void StochasticMethod::createTree()
+    {
+        Polyhedron poly = extractPolyhedron();
+        TriangleList triangles = getTriangleList(poly);
+
+        tree = new Tree(triangles.begin(), triangles.end());
+    }
+
+    Polyhedron StochasticMethod::extractPolyhedron()
+    {
+        std::ifstream meshIn(meshFile);
+        Polyhedron poly;
+        meshIn >> poly;
+        meshIn.close();
+
+        return poly;
+    }
+
+    TriangleList StochasticMethod::getTriangleList(Polyhedron &poly)
+    {
+        TriangleList triangles;
+        for (Facet_iterator it = poly.facets_begin(); it != poly.facets_end(); it++) {
+            Halfedge_facet_circulator p = it->facet_begin();
+            Vertex p0 = p->vertex();
+            Vertex p1 = (++p)->vertex();
+            Vertex p2 = (++p)->vertex();
+
+            triangles.push_back(Triangle(p0->point(), p1->point(), p2->point()));
+        }
+        return triangles;
+    }
+
+    TreePtr StochasticMethod::getTree()
+    {
+        return tree;
+    }
+
+    GLMVec3List StochasticMethod::getCams()
+    {
+        return cams;
+    }
+
+    CameraGeneralConfiguration StochasticMethod::getCamConfig()
+    {
+        return camConfig;
+    }
+
+    StochasticConfiguration StochasticMethod::getStochasticConfig()
+    {
+        return stoConfig;
+    }
+
+    size_t StochasticMethod::getUniformParticles()
+    {
+        return stoConfig.particles.uniform;
+    }
+
+    size_t StochasticMethod::getResamplingParticles()
+    {
+        return stoConfig.particles.num;
+    }
+
+    size_t StochasticMethod::getResamplingSteps()
+    {
+        return stoConfig.resamplingNum;
+    }
+
+    VonMisesConfiguration StochasticMethod::getVonMisesConfig()
+    {
+        return vonMisesConfig;
+    }
+
+    FormulationPtr StochasticMethod::getFormulation()
+    {
+        return formulation;
+    }
+
+    void StochasticMethod::setOffspring(double offspring)
+    {
+        this->OFFSPRING = offspring;
+    }
+
+    double StochasticMethod::getOffspring()
+    {
+        return OFFSPRING;
+    }
+    
+    GLMVec3 StochasticMethod::lowerBounds()
+    {
+        return stoConfig.bounds.lower;
+    }
+
+    GLMVec3 StochasticMethod::upperBounds()
+    {
+        return stoConfig.bounds.upper;
+    }
+
+    float StochasticMethod::offsetX()
+    {
+        return stoConfig.bounds.lower.x;
+    }
+
+    float StochasticMethod::offsetY()
+    {
+        return stoConfig.bounds.lower.y;
+    }
+
+    float StochasticMethod::offsetZ()
+    {
+        return stoConfig.bounds.lower.z;
+    }
+
+    ExhaustiveReportWriterPtr StochasticMethod::getLogger()
+    {
+        return log;
+    }
+
+    void StochasticMethod::setLogger(ExhaustiveReportWriterPtr log)
+    {
+        delete this->log;
+        this->log = log;
+    }
+    
+} // namespace opview

1830cf642fa7017207faee62589fea93ff90dba1
Luca Morreale <luca.morreale93@gmail.com>
2017-09-16 16:10:19 +0200

    Optimal cam: create abstract class for autonomous stocastic methods
    Create basic abstract class for methods that autonomously identify a set of centroids and optimize energy wrt them using Formulation class

---
+++ cam_position_generator/include/opview/AutonomousStochasticMethod.hpp
@@ -0,0 +1,57 @@
+#ifndef CAM_POSITION_GENERATOR_AUTONOMOUS_STOCHASTIC_METHOD_H
+#define CAM_POSITION_GENERATOR_AUTONOMOUS_STOCHASTIC_METHOD_H
+
+#include <opview/NotImplementedMethodException.hpp>
+#include <opview/StochasticMethod.hpp>
+
+namespace opview {
+
+    
+    class AutonomousStochasticMethod : public StochasticMethod {
+    public:
+        AutonomousStochasticMethod(CameraGeneralConfiguration &camConfig, MeshConfiguration &meshConfig, 
+                                    StochasticConfiguration &stoConfig, size_t maxPoints, double goalAngle=45, double dispersion=5);
+
+        ~AutonomousStochasticMethod();
+
+        virtual DoubleList estimateBestCameraPosition() = 0;
+        virtual DoubleList estimateBestCameraPosition(GLMVec3 &centroid, GLMVec3 &normVector);
+
+        GLMVec3List getPoints();
+        GLMVec3List getNormals();
+        DoubleList getUncertainties();
+        void updateMeshInfo(int pointIndex, GLMVec3 point, GLMVec3 normal, double uncertainty);
+        void updateMeshInfo(int pointIndex, GLMVec3 normal, double uncertainty);
+        void updateMeshInfo(int pointIndex, double uncertainty);
+        void addPoint(GLMVec3 point, GLMVec3 normal, double uncertainty);
+
+        
+    protected:
+
+        OrderedPose uniformSamplingStep(GLMVec3List &centroids, GLMVec3List &normals, int round);
+        OrderedPose computeEnergyForPoses(GLMVec3List &centroids, GLMVec3List &normals, EigVector5List &orientedPoints);
+
+    
+
+        virtual void setupWorstPoints();
+        virtual void updateWorstPoints(int index, long double uncertainty);
+        virtual void retainWorst();
+        virtual GLMVec3ListPair getWorstPointsList();
+
+    
+
+        
+        
+    private:
+        GLMVec3List points;
+        GLMVec3List normals;
+        DoubleList uncertainty;
+        size_t maxPoints;
+
+        DoubleIntList worstPointsList;
+        
+    };
+
+} // namespace opview
+
+#endif // CAM_POSITION_GENERATOR_AUTONOMOUS_STOCHASTIC_METHOD_H
---
+++ cam_position_generator/src/AutonomousStochasticMethod.cpp
@@ -0,0 +1,123 @@
+#include <opview/AutonomousStochasticMethod.hpp>
+
+namespace opview {
+
+    AutonomousStochasticMethod::AutonomousStochasticMethod(CameraGeneralConfiguration &camConfig, 
+                MeshConfiguration &meshConfig, StochasticConfiguration &stoConfig, size_t maxPoints, double goalAngle, double dispersion)
+                : StochasticMethod(camConfig, meshConfig.filename, meshConfig.cams, stoConfig, goalAngle, dispersion)
+    {
+        this->points = meshConfig.points;
+        this->normals = meshConfig.normals;
+        this->uncertainty = meshConfig.uncertainty;
+        this->maxPoints = maxPoints;
+
+        setupWorstPoints();
+
+        this->getLogger()->resetFile("auto_stochastic.json");
+    }
+
+    AutonomousStochasticMethod::~AutonomousStochasticMethod()
+    { /*    */ }
+
+    DoubleList AutonomousStochasticMethod::estimateBestCameraPosition(GLMVec3 &centroid, GLMVec3 &normVector)
+    {
+        throw NotImplementedMethodException("Method not implemented in Autonomous version");
+    }
+
+
+    OrderedPose AutonomousStochasticMethod::uniformSamplingStep(GLMVec3List &centroids, GLMVec3List &normals, int round)
+    {
+        EigVector5List orientedPoints = uniformPointsGetter();
+        OrderedPose poses = computeEnergyForPoses(centroids, normals, orientedPoints);
+        return this->extractBestResults(poses, round);
+    }
+
+    OrderedPose AutonomousStochasticMethod::computeEnergyForPoses(GLMVec3List &centroids, GLMVec3List &normals, EigVector5List &orientedPoints)
+    {
+        DoubleList values(orientedPoints.size());
+
+        #pragma omp parallel for
+        for (int i = 0; i < orientedPoints.size(); i++) {
+            values[i] = getFormulation()->computeEnergy(orientedPoints[i], centroids, normals);
+        }
+
+        return orderPoses(orientedPoints, values);
+    }
+
+    void AutonomousStochasticMethod::updateMeshInfo(int pointIndex, GLMVec3 point, GLMVec3 normal, double uncertainty)
+    {
+        this->points[pointIndex] = point;
+        updateMeshInfo(pointIndex, normal, uncertainty);
+    }
+
+    void AutonomousStochasticMethod::updateMeshInfo(int pointIndex, GLMVec3 normal, double uncertainty)
+    {
+        this->normals[pointIndex] = normal;
+        updateMeshInfo(pointIndex, uncertainty);
+    }
+
+    void AutonomousStochasticMethod::updateMeshInfo(int pointIndex, double uncertainty)
+    {
+        this->uncertainty[pointIndex] = uncertainty;
+        updateWorstPoints(pointIndex, uncertainty);
+    }
+
+    void AutonomousStochasticMethod::addPoint(GLMVec3 point, GLMVec3 normal, double uncertainty)
+    {
+        this->points.push_back(point);
+        this->normals.push_back(normal);
+        this->uncertainty.push_back(uncertainty);
+    }
+
+    void AutonomousStochasticMethod::setupWorstPoints()
+    {
+        worstPointsList.clear();
+        for (int p = 0; p < points.size(); p++) {
+            worstPointsList.push_back(std::make_pair(uncertainty[p], p));
+        }
+        retainWorst();
+    }
+
+    void AutonomousStochasticMethod::updateWorstPoints(int index, long double uncertainty)
+    {
+        worstPointsList.push_back(std::make_pair(uncertainty, index));
+        retainWorst();
+    }
+
+    void AutonomousStochasticMethod::retainWorst()
+    {
+        std::sort(worstPointsList.rbegin(), worstPointsList.rend());
+        int eraseFrom = std::min(this->maxPoints, worstPointsList.size());
+        worstPointsList.erase(worstPointsList.begin() + eraseFrom, worstPointsList.end());
+    }
+
+    GLMVec3ListPair AutonomousStochasticMethod::getWorstPointsList()
+    {
+        GLMVec3List worstPoints;
+        GLMVec3List worstNormals;
+
+        for (int i = 0; i < worstPointsList.size(); i++) {
+            int index = worstPointsList[i].second;
+            worstPoints.push_back(this->points[index]);
+            worstNormals.push_back(this->normals[index]);
+        }
+
+        return std::make_pair(worstPoints, worstNormals);
+    }
+
+    GLMVec3List AutonomousStochasticMethod::getPoints()
+    {
+        return points;
+    }
+
+    GLMVec3List AutonomousStochasticMethod::getNormals()
+    {
+        return normals;
+    }
+    
+    DoubleList AutonomousStochasticMethod::getUncertainties()
+    {
+        return uncertainty;
+    }
+
+} // namespace opview

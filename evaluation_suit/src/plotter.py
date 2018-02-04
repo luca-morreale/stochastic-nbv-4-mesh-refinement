98a989c14eafba330e14547655e8c25503dcfa2f
Luca Morreale <luca.morreale93@gmail.com>
2018-02-04 11:18:53 +0100

    Eval: fix plotter python script

--- evaluation_suit/src/plotter.py
+++ evaluation_suit/src/plotter.py
@@ -1,4 +1,4 @@
-
+import sys
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import savefig
 
@@ -6,20 +6,24 @@ from matplotlib.pyplot import savefig
 from os import listdir
 from os.path import isfile, join
 
-onlyfiles = [f for f in listdir(".") if isfile(f)]
-onlyfiles = [f for f in onlyfiles if f.find("_coverage.txt") != -1]
+if len(sys.argv) < 2:
+    print "usage %s: file_coverage.txt"%(sys.argv[0])
+    sys.exit(0)
 
-for file in onlyfiles:
 
-    with open(file) as stream:
-        content = stream.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
-    ascissa = [float(x.strip().split(" ")[0]) for x in content] 
-    content = [float(x.strip().split(" ")[1]) for x in content] 
+file = sys.argv[1]
 
-    # create the general figure     
-    plt.plot(ascissa, content)
-    plt.ylabel("% Coverage")
-    plt.xlabel("Distance Threshold")
-    savefig(file + ".pdf")
-    plt.clf()
+with open(file) as stream:
+    content = stream.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [(float(x.strip().split(" ")[0]), float(x.strip().split(" ")[1])) for x in content]
+#content = [float(x.strip().split(" ")[1]) for x in content] 
+print content
+
+# create the general figure     
+plt.plot([x[0] for x in content], [x[1] for x in content])
+plt.ylabel("% Coverage")
+plt.xlabel("Views")
+savefig(file + ".png")
+plt.clf()
+
